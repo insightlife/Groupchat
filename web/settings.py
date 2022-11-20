4237a00db3a8e42b693b9a0e3905b1dba8f565b7
insightlife <112997689+insightlife@users.noreply.github.com>
2022-11-20 09:45:22 +0530

    update

--- web/settings.py
+++ web/settings.py
@@ -107,5 +107,5 @@ CHANNEL_LAYERS = {
 
 AUTHENTICATION_BACKENDS = (('django.contrib.auth.backends.ModelBackend'),)
 
-CSRF_TRUSTED_ORIGINS = ["https://groupchat98.herokuapp.com","https://127.0.0.1:8000/","https://chat-webapp20.herokuapp.com/"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000/"]
 
